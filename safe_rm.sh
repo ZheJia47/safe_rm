88eab4c22bbf3c85c2308921ab1308342e63cd48
ZheJia47 <53133006+ZheJia47@users.noreply.github.com>
2020-09-05 19:02:48 +0800

    Initial commit

---
+++ safe_rm.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+dir=/Users/zhangzhejia14/.Trash # trash can pwd
+
+mv $@ $dir
+
+#declare -i count=$#+1
+#for i in $(seq 1 $count);
+#do
+#    if [ -d "$i" -o -f "$i" ];then
+ #           name=`basename $i`    
+#	    echo $name
+ #   fi
+#done
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
